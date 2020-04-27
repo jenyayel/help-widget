9d3a283892210d13a093b419144e6a7f9a12f760
Jenya Y <jenyay@gmail.com>
2020-04-28 00:15:47 +0300

    Configurations model

--- src/models.ts
+++ src/models.ts
@@ -4,10 +4,29 @@ interface InfraConfigurations {
 
 export interface AppConfigurations {
     debug: boolean;
+    serviceBaseUrl: string;
+    minimized: boolean;
+    disableDarkMode: boolean;
+    text: {
+        minimizedTitle?: string;
+        formTitle?: string;
+        formSubTitle?: string;
+        thankYouTitle?: string;
+        thankYouBody?: string;
+        faqTitle?: string;
+    };
+    styles: {
+        classNameContainer?: string;
+    };
 }
 
 export type Configurations = InfraConfigurations & AppConfigurations;
 
 export const defaultConfig: Configurations = {
     debug: false,
+    serviceBaseUrl: '',
+    minimized: false,
+    disableDarkMode: false,
+    text: {},
+    styles: {}
 };
