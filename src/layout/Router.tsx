34e1c2c9cb72c7dc9b9dbe111e44fd9e8035eb77
Jenya Y <jenyay@gmail.com>
2020-04-28 13:18:37 +0300

    Comments for router

--- src/layout/Router.tsx
+++ src/layout/Router.tsx
@@ -4,6 +4,9 @@ import { useState, useEffect } from 'preact/hooks';
 const DEFAULT_ROUTE = '/';
 
 interface Props {
+    /**
+     * Specifies all URLs and their respectful components.
+     */
     routes: {
         [DEFAULT_ROUTE]: VNode;
         [key: string]: VNode;
@@ -11,9 +14,15 @@ interface Props {
     onChange?: (route: string) => void;
 }
 
+/**
+ * Stores current URL of the router and allows to change it programmatically.
+ */
 export const RouterContext = createContext<{ route: string, setRoute: (route: string) => void }>(
     { route: DEFAULT_ROUTE, setRoute: (_: string) => undefined });
 
+/**
+ * Oversimplified router component.
+ */
 export const Router = ({ routes, onChange }: Props) => {
     const [route, setRoute] = useState(DEFAULT_ROUTE);
     useEffect(() => onChange?.(route), [route]);
@@ -28,6 +37,11 @@ export const Router = ({ routes, onChange }: Props) => {
 export const RouteComponent = (props: { component: ComponentType<null> }) =>
     createElement(props.component, null);
 
+/**
+ * Render anchor with click handler to switch route based on `href` attribute.
+ * We intentionally override final `href`, so links within widget won't lead to actual
+ * pages on website.
+ */
 export const RouteLink = ({ href, children, ...rest }: h.JSX.HTMLAttributes<HTMLAnchorElement>) => (
     <RouterContext.Consumer>
         {({ setRoute }) => (
