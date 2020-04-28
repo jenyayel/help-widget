930c788eb6be2dcc24d12f64bda77d45d861671f
Jenya Y <jenyay@gmail.com>
2020-04-28 13:07:44 +0300

    Title bar component

---
+++ src/components/TitleBar.tsx
@@ -0,0 +1,33 @@
+import { h } from 'preact';
+import style from './titlebar.css';
+import { useContext, useState, useEffect } from 'preact/hooks';
+import { ConfigContext } from '../context';
+
+interface OwnProps {
+    onOpen?: () => void;
+    onMinimize?: () => void;
+    routeTitle: string;
+}
+const TitleBar = (props: OwnProps) => {
+    const config = useContext(ConfigContext);
+    const [minimized, setMinimized] = useState(config.minimized);
+    useEffect(() => {
+        if (minimized) {
+            props.onMinimize?.();
+        } else {
+            props.onOpen?.();
+        }
+    }, [minimized, props.onMinimize, props.onOpen]);
+
+    return (
+        <div className={style.root} onClick={() => setMinimized(!minimized)}>
+            <h4>{minimized
+                ? config.text.minimizedTitle ?? 'Help'
+                : props.routeTitle}</h4>
+            <a
+                className={minimized ? style.minimized : style.open}
+                title={minimized ? 'Open' : 'Minimize'} />
+        </div>);
+};
+
+export default TitleBar;
