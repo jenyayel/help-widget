a973b82c989f2ff9ebd26b00d03777c142ba5352
Jenya Y <jenyay@gmail.com>
2020-05-01 00:56:27 +0300

    Formatting

--- webpack.config.js
+++ webpack.config.js
@@ -4,86 +4,93 @@ var copyWebpackPlugin = require('copy-webpack-plugin');
 const bundleOutputDir = './dist';
 
 module.exports = (env) => {
-    const isDevBuild = !(env && env.prod);
+  const isDevBuild = !(env && env.prod);
 
-    return [{
-        entry: './src/index.ts',
-        output: {
-            filename: 'widget.js',
-            path: path.resolve(bundleOutputDir),
-        },
-        devServer: {
-            contentBase: bundleOutputDir
-        },
-        plugins: isDevBuild
-            ? [new webpack.SourceMapDevToolPlugin(), new copyWebpackPlugin([{ from: 'dev/' }])]
-            : [],
-        optimization: {
-            minimize: !isDevBuild
-        },
-        mode: isDevBuild ? 'development' : 'production',
-        module: {
-            rules: [
-                // packs SVG's discovered in url() into bundle
-                { test: /\.svg/, use: 'svg-url-loader' },
-                // allows import CSS as modules
-                {
-                    test: /\.css$/i,
-                    use: [
-                        {
-                            loader: 'style-loader',
-                            options: { injectType: 'singletonStyleTag' }
-                        },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: {
-                                    localIdentName: '[name]-[local]-[hash:base64:5]'
-                                },
-                                sourceMap: isDevBuild
-                            }
-                        }
-                    ]
+  return [{
+    entry: './src/index.ts',
+    output: {
+      filename: 'widget.js',
+      path: path.resolve(bundleOutputDir),
+    },
+    devServer: {
+      contentBase: bundleOutputDir
+    },
+    plugins: isDevBuild
+      ? [new webpack.SourceMapDevToolPlugin(), new copyWebpackPlugin([{ from: 'dev/' }])]
+      : [],
+    optimization: {
+      minimize: !isDevBuild
+    },
+    mode: isDevBuild ? 'development' : 'production',
+    module: {
+      rules: [
+        // packs SVG's discovered in url() into bundle
+        { test: /\.svg/, use: 'svg-url-loader' },
+        {
+          test: /\.css$/i,
+          use: [
+            {
+              loader: 'style-loader',
+              options: { injectType: 'singletonStyleTag' }
+            },
+            {
+              // allows import CSS as modules
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  // css class names format
+                  localIdentName: '[name]-[local]-[hash:base64:5]'
                 },
-                // use babel-loader for TS and JS modeles,
-                // starting v7 Babel babel-loader can transpile TS into JS,
-                // so no need for ts-loader
-                // note, that in dev we still use tsc for type checking
-                {
-                    test: /\.(js|ts|tsx|jsx)$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: [
-                                    ['@babel/preset-env', {
-                                        'targets': {
-                                            'browsers': ['IE 11, last 2 versions']
-                                        },
-                                        // makes usage of @babel/polyfill because of IE11
-                                        useBuiltIns: 'usage'
-                                    }],
-                                    ["@babel/typescript", { jsxPragma: "h" }]
-                                ],
-                                'plugins': [
-                                    // syntax sugar found in React components
-                                    '@babel/proposal-class-properties',
-                                    '@babel/proposal-object-rest-spread',
-                                    // transpile JSX/TSX to JS
-                                    ['@babel/plugin-transform-react-jsx', {
-                                        // we use Preact, which has `Preact.h` instead of `React.createElement`
-                                        pragma: 'h',
-                                        pragmaFrag: 'Fragment'
-                                    }]
-                                ]
-                            }
-                        }
-                    ]
-                }]
+                sourceMap: isDevBuild
+              }
+            }
+          ]
         },
-        resolve: {
-            extensions: ['*', '.js', '.ts', '.tsx']
-        }
-    }];
+        // use babel-loader for TS and JS modeles,
+        // starting v7 Babel babel-loader can transpile TS into JS,
+        // so no need for ts-loader
+        // note, that in dev we still use tsc for type checking
+        {
+          test: /\.(js|ts|tsx|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', {
+                    'targets': {
+                      'browsers': ['IE 11, last 2 versions']
+                    },
+                    // makes usage of @babel/polyfill because of IE11
+                    // there is at least async functions and for..of
+                    useBuiltIns: 'usage'
+                  }],
+                  [
+                    // enable transpiling ts => js
+                    "@babel/typescript",
+                    // tell babel to compile JSX using into Preact
+                    { jsxPragma: "h" }
+                  ]
+                ],
+                'plugins': [
+                  // syntax sugar found in React components
+                  '@babel/proposal-class-properties',
+                  '@babel/proposal-object-rest-spread',
+                  // transpile JSX/TSX to JS
+                  ['@babel/plugin-transform-react-jsx', {
+                    // we use Preact, which has `Preact.h` instead of `React.createElement`
+                    pragma: 'h',
+                    pragmaFrag: 'Fragment'
+                  }]
+                ]
+              }
+            }
+          ]
+        }]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.ts', '.tsx']
+    }
+  }];
 };
